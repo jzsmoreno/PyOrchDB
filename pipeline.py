4c528848c45ac33cb8cc543ebe26d8d31987a350
jzsmoreno <42299052+jzsmoreno@users.noreply.github.com>
2023-08-21 16:35:39 -0600

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -17,20 +17,44 @@ ps_command += "\n"
 ps_command += "\n"
 ps_command += "if (az group exists --resource-group $resourceGroupName){\n"
 ps_command += "\techo "
-ps_command += "'Resource group $resourceGroupName already exists.'\n"
+ps_command += "'Resource group already exists.'\n"
 ps_command += "}\n"
 ps_command += "else{\n\t"
 ps_command += "echo "
-ps_command += "'Resource group $resourceGroupName does not exist. Creating...'\n\t"
+ps_command += "'Resource group does not exist. Creating...'\n\t"
 ps_command += f"az group create --name $resourceGroupName --location {yaml_data['location']}"
 ps_command += "\n\t"
 ps_command += f"az ml workspace create --resource-group {yaml_data['resource_group_name']}"
 ps_command += f" --name {yaml_data['workspace_name']}"
 ps_command += f" --location {yaml_data['location']}"
-ps_command += "\n}"
+ps_command += "\n}\n"
+
+py_command = "print('Hello World!')"
 
 
 if __name__ == "__main__":
+    # create run_workflow.py
+    with open("run_workflow.py", "w") as file:
+        file.write(py_command)
+
+    # get storage account name
+    ps_command += "$storageName = (az ml datastore list --query '[0].{storageName:account_name}' "
+    ps_command += "--output table "
+    ps_command += f"--workspace-name {yaml_data['workspace_name']} "
+    ps_command += f"--resource-group {yaml_data['resource_group_name']})[2]"
+    # get storage account key
+    ps_command += "\n"
+    ps_command += "$storageKey = (az storage account keys list --resource-group "
+    ps_command += (
+        f"{yaml_data['resource_group_name']} --account-name $storageName --query '[0].value'"
+    )
+    ps_command += " --output tsv)\n"
+    ps_command += "az storage blob upload --account-name "
+    ps_command += f"$storageName --account-key $storageKey --container-name azureml "
+    ps_command += (
+        f"--file ./{yaml_data['script_name']} --name {yaml_data['script_name']}"
+    )
+
     # save command_line into a run.ps1 file
     with open("run.ps1", "w") as file:
         file.write(ps_command)
--- run.ps1
+++ run.ps1
@@ -2,10 +2,13 @@ az login
 $resourceGroupName='test'
 
 if (az group exists --resource-group $resourceGroupName){
-	echo 'Resource group $resourceGroupName already exists.'
+	echo 'Resource group already exists.'
 }
 else{
-	echo 'Resource group $resourceGroupName does not exist. Creating...'
+	echo 'Resource group does not exist. Creating...'
 	az group create --name $resourceGroupName --location eastus
 	az ml workspace create --resource-group test --name testazml --location eastus
 }
+$storageName = (az ml datastore list --query '[0].{storageName:account_name}' --output table --workspace-name testazml --resource-group test)[2]
+$storageKey = (az storage account keys list --resource-group test --account-name $storageName --query '[0].value' --output tsv)
+az storage blob upload --account-name $storageName --account-key $storageKey --container-name azureml --file ./run_workflow.py --name run_workflow.py
