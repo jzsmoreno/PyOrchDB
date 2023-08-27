0029453b7818eafaeeacb1100abbfc9c91f1e502
jzsmoreno <42299052+jzsmoreno@users.noreply.github.com>
2023-08-26 20:16:07 -0600

    Major improvements
    * Added features
    * Update run_workflow.py
    * Update pipeline.py

--- run.ps1
+++ run.ps1
@@ -5,6 +5,13 @@ if (az group exists --resource-group $resourceGroupName){
 	echo 'Resource group already exists.'
 	if ($False){
 		try{
+			az ml workspace delete --resource-group test --name testazml --permanently-delete -y
+		}		
+		catch{
+			 continue
+		}
+
+		try{
 			while ($true){
 				az resource delete --name (az resource list --query '[0].{name:name}' --resource-group test --output table)[2] --resource-type (az resource list --query [0]'.{name:type}' --resource-group test --output table)[2] --resource-group test
 				Start-Sleep -Seconds 1.5
@@ -24,7 +31,7 @@ else{
 }
 echo 'Extraction and transformation. Running...'
 $storageBlob_conn = (az storage account show-connection-string --name testsa --resource-group test --query 'connectionString' --output tsv)
-python ./run_workflow.py testsa, $storageBlob_conn, test
+Start-Process python -ArgumentList './run_workflow.py', 'testsa', $storageBlob_conn, 'rawdata', 'test' -NoNewWindow -Wait
 $storageName = (az ml datastore list --query '[0].{storageName:account_name}' --output table --workspace-name testazml --resource-group test)[2]
 $storageKey = (az storage account keys list --resource-group test --account-name $storageName --query '[0].value' --output tsv)
 az storage blob upload --account-name $storageName --account-key $storageKey --container-name azureml --file ./jobs/job_workflow.py --name job_workflow.py --overwrite
--- run_workflow.py
+++ run_workflow.py
@@ -1,1 +1,21 @@
-print("Hello World!")
+import sys
+
+from pydbsmgr.utils.azure_sdk import StorageController
+
+print("Start run_workflow.py")
+
+if __name__ == "__main__":
+    print("line arguments : ", len(sys.argv))
+    print(sys.argv)
+    storage_name = sys.argv[1]
+    conn_string = sys.argv[2]
+    container_name = sys.argv[3]
+    resource_group_name = sys.argv[4]
+
+    controller = StorageController(conn_string, container_name)
+    files = controller.get_all_blob()
+    for file in files:
+        print("File name : ", file)
+
+    with open("output.txt", "w") as outfile:
+        outfile.writelines(files)
