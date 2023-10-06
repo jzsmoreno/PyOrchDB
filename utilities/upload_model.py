ff2b6d8c9c3bd8844a1524204147d92e6bda585f
alexis31c <77429775+alexis31c@users.noreply.github.com>
2023-10-06 17:10:21 -0600

    development (#34)
    * Added insert_column_period function
    
    * Update code-quality.yml
    
    * Create requirements.txt
    
    * Update code-quality.yml
    
    * Update requirements.txt
    
    * Update code-quality.yml
    
    * Update code-quality.yml
    
    * Update code-quality.yml
    
    * add a complete regex
    
    * reformatting
    
    * feature/reformatting (#3)
    
    * Update run_workflow.py
    
    * Update code-quality.yml
    
    * Update run_workflow.py
    
    * Delete utils.py
    
    * Update run_workflow.py
    
    * bug fix
    
    * feature/upload-parquet (#5)
    
    * Update run_workflow.py
    
    * document improvements
    * implementation of LightCleaner
    
    * Update run_workflow.py
    
    * bug fix
    
    * Update run_workflow.py
    
    * Improvement of names
    
    * Add catalog functionality (#4)
    
    * Update run_workflow.py
    
    * Bug fixes
    
    * Update run_workflow.py
    
    * Script to upload a model to `Azure ML`
    
    * How to implement the main class to upload a model
    
    * new branch of `development`
    
    * Update `upload_model.py`
    
    * refactoring
    
    ---------
    
    Co-authored-by: nirvana43p <equilibrio.top15@gmail.com>
    Co-authored-by: jzsmoreno <42299052+jzsmoreno@users.noreply.github.com>
    Co-authored-by: J. Ivan Avalos <75811216+nirvana43p@users.noreply.github.com>
    Co-authored-by: MajoFons21 <41271799+MajFons@users.noreply.github.com>

---
+++ utilities/upload_model.py
@@ -0,0 +1,44 @@
+import os
+
+from azureml.core import Environment, Model, Workspace
+from azureml.core.webservice import AciWebservice
+
+
+class UploadModelToML:
+    """Performs the upload of a `serialized` Machine Learning model to `Azure ML`."""
+
+    def __init__(self, subscription_id, resource_group, workspace_name):
+        """Inputs required to create a connection to `Azure ML`"""
+        self._subscription_id = subscription_id
+        self._resource_group = resource_group
+        self._workspace_name = workspace_name
+
+    def upload_model(self, model_path, model_name):
+        """Create a connection to the `azure ml` resource
+        and to load the model you need to add the path and name of your model."""
+        try:
+            ws = Workspace(
+                subscription_id=self._subscription_id,
+                resource_group=self._resource_group,
+                workspace_name=self._workspace_name,
+            )
+            print("Successful connection to Azure Machine Learning.")
+            modelo_cargado = Model.register(
+                workspace=ws, model_path=model_path, model_name=model_name, tags={"type": "pkl"}
+            )
+            print(".pkl model successfully registered in Azure Machine Learning.")
+        except Exception as e:
+            print(f"Error connecting to Azure Machine Learning: {str(e)}")
+
+
+if __name__ == "__main__":
+    subscription_id = "<suscription_id>"
+    resource_group = "<resource_group_name>"
+    workspace_name = "<workspace_name>"
+    model_name = "<model_name>"
+
+    # Gets the absolute path of the .pkl model relative to the script
+    model_path = os.path.join(os.path.dirname(__file__), model_name + ".pkl")
+
+    azureml_controller = UploadModelToML(subscription_id, resource_group, workspace_name)
+    azureml_controller.upload_model(model_name=model_name, model_path=model_path)
