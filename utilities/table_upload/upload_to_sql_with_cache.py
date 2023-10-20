bfd2e995f7a6258a1251edb80b6cf614adf649b4
jzsmoreno <42299052+jzsmoreno@users.noreply.github.com>
2023-10-20 14:14:55 -0600

    Update `upload_to_sql_with_cache.py`

--- utilities/table_upload/upload_to_sql_with_cache.py
+++ utilities/table_upload/upload_to_sql_with_cache.py
@@ -58,8 +58,7 @@ def add_cache(cache: set, data_set: set) -> set:
     Returns:
         set: the cache with the updated information
     """
-    for item in data_set:
-        cache.add(item)
+    cache.update(data_set)
     return cache
 
 
@@ -73,15 +72,12 @@ def is_duplicate(cache: set, data: list) -> Tuple[bool, list, set]:
     Returns:
         Tuple[`bool`, `list`, `set`]: indicates whether inserts were previously performed, the difference of the inserts and their set.
     """
-    data_set = set()
-    for row in data:
-        data_set.add(tuple(row))
+    data_set = set(tuple(row) for row in data)
 
     data_set = data_set.difference(cache)
     data = []
     if data_set:
-        for item in data_set:
-            data.append(item)
+        data.extend(data_set)
         return False, data, data_set
     else:
         return True, data, data_set
@@ -103,6 +99,7 @@ class UploadToSQL:
         auto_resolve: bool = True,
         frac: float = 0.01,
         chunk_size: int = 20,
+        interruption_mode: bool = False,
         max_retries: int = 3,
         retry_delay: int = 5,
         cache_file_path: str = "./logs/cache.pkl",
@@ -163,10 +160,18 @@ class UploadToSQL:
                             ]
                             for row in chunk.values.tolist()
                         ]
-                        duplicate_data, data, data_set = is_duplicate(cache, data)
-                        if duplicate_data:
-                            print("Data already exists, skipping insert.")
-                            break
+
+                        if interruption_mode:
+                            duplicate_data, data, data_set = is_duplicate(cache, data)
+                            if duplicate_data:
+                                print("Data already exists, skipping insert.")
+                                break
+                        else:
+                            if attempt > 0:
+                                duplicate_data, data, data_set = is_duplicate(cache, data)
+                                if duplicate_data:
+                                    print("Data already exists, skipping insert.")
+                                    break
 
                         cur.fast_executemany = True
                         cur.executemany(
@@ -180,6 +185,7 @@ class UploadToSQL:
 
                         print(chunk)
                         print(file_name[0])
+                        data_set = set(tuple(row) for row in data)
                         cache = add_cache(cache, data_set)
                         save_cache(cache_file_path, cache)
                         break
